88965b06bf042a7b6ea2eac81a24cc0ec438daf9
AndreasPflaume <andyreas.pa@gmail.com>
2024-02-06 15:29:12 +0100

    css revised and js

--- contacts.js
+++ contacts.js
@@ -1,30 +1,38 @@
-function newContact(){
-    let newContact = document.getElementById('newContact');
-    newContact.innerHTML =`
-    <div class="backroundNewContactContainer">
-        <div class="newContactContainer">
-            <div class="addContact">
-                <img src="" alt="Bild Join">
-                <h1>Add contact</h1>
-                <p> Tasks are better with a team!</p>
-            </div>
-            <div class="pictureAndFormContact">
-                <div class="pictureNewContact">
-                    <img src="" alt="Bild Contact">
-                </div>
-                <div>
-                    <form action="formContact">
-                        <input type="text" placeholder="Name"> <br>
-                        <input type="email" placeholder="Email" > <br>
-                        <input type="number" placeholder="Phone"> <br>
-                        <button>Cancel x</button>
-                        <button class="buttonContact">Create contact</button>
-                    </form>
-                </div>
-            
-            </div>
-
-        </div>
-    </div>`;
+let contacts = [
+    {
+        "name": "Andreas Pflaum",
+        "e-mail": "andy@gmail.com",
+        "tel": "0157733562"
+    },
+    {
+        'name': 'Lydia Lehnert',
+        'e-mail': 'lydia@gmail.com',
+        'tel': '0157742434'
+    },
+    {
+        'name': 'Dimitrios Kapetanis',
+        'e-mail': 'dimi@gmail.com',
+        'tel': '0157745677'
+    },
     
+];
+
+function newContactOpen() {
+    let backround = document.getElementById('backround');
+    backround.classList.remove('d-none');
+
+}
+
+function closeContact() {
+    let backround = document.getElementById('backround');
+    backround.classList.add('d-none');
+}
+
+function contactList(){
+    let list = document.getElementById('newContacts');
+    list.innerHTML = '';
+     for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        
+     }
 }
