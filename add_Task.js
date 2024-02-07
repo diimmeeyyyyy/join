c5f1e5e834b2b33f978eb8ef37de53d13a9a61af
EndlessLoop88 <VanillaButterfly@web.de>
2024-02-07 16:35:14 +0100

    section add task: added function addPrioToTask

--- add_Task.js
+++ add_Task.js
@@ -1,4 +1,11 @@
 let tasks = [];
+let prio = '';
+
+
+function addPrioToTask(priority){
+    prio = priority;
+    return prio; 
+}
 
 
 function addToTasks() {
@@ -6,12 +13,8 @@ let title = document.getElementById('add_task_title');
 let description = document.getElementById('add_task_description');
 let contactsToAssign = document.getElementById('add_task_contacts_to_assign');
 let dueDate = document.getElementById('add_task_due_date');
-// gewählte Prio der Buttons erfassen
 let category = document.getElementById('add_task_categorie');
 let subtasks = document.getElementById('add_task_subtasks');
-let urgentButton = document.getElementById('add_task_prio_button_urgent');
-let mediumButton = document.getElementById('add_task_prio_button_medium');
-let lowButton = document.getElementById('add_task_prio_button_low');
 
 
 let task = {
@@ -19,12 +22,13 @@ let task = {
     "description": description.value,
     "contactsToAssign":contactsToAssign.value,
     "dueDate": dueDate.value,
+    "prio": prio,
     "category": category.value,
     "subtasks": subtasks.value
 }
 
 tasks.push(task); 
-console.log(tasks);
+console.log('tasks', tasks);
 
 title.value = '';
 description.value = '';
@@ -32,10 +36,6 @@ contactsToAssign.value ='';
 dueDate.value = '';
 category.value = '';
 subtasks.value = ''; 
-// urgentButton.style.backgroundColor = "white";
-// mediumButton.style.backgroundColor = "white";
-// lowButton.style.backgroundColor = "white";
-
 }
 
 
