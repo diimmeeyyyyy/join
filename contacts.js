03b4da8acc33c0de133779e6706c44085d977021
AndreasPflaume <andyreas.pa@gmail.com>
2024-02-08 10:36:59 +0100

    js for contacts

--- contacts.js
+++ contacts.js
@@ -5,17 +5,28 @@ let contacts = [
         "tel": "0157733562"
     },
     {
-        'name': 'Lydia Lehnert',
-        'e-mail': 'lydia@gmail.com',
-        'tel': '0157742434'
+        "name":'Lydia Lehnert',
+        "e-mail":'lydia@gmail.com',
+        "tel":'0157742434'
+
     },
     {
-        'name': 'Dimitrios Kapetanis',
-        'e-mail': 'dimi@gmail.com',
-        'tel': '0157745677'
-    },
-    
+        "name":'Dimitrios Kapetanis',
+        "e-mail":'dimi@gmail.com',
+        "tel": '0157745677'
+    }
+ 
 ];
+
+let letters = contacts.map(contact => contact.name.charAt(0)); // Erster Buchstabe vom Array contacts['name'] wird übernommen!
+
+let twolettersName = contacts.map(contact =>{                   // Zwei Buchstaben werden übergeben zb. May Musstermann = MM !
+    const nameSplit = contact.name.split(' ');
+    const twoNummber = nameSplit.map(teil => teil.charAt(0));
+    return twoNummber.join('');
+});
+
+
 
 function newContactOpen() {
     let backround = document.getElementById('backround');
@@ -33,6 +44,21 @@ function contactList(){
     list.innerHTML = '';
      for (let i = 0; i < contacts.length; i++) {
         const contact = contacts[i];
+        list.innerHTML +=`
+        <div>
+        <div class="name-letter">${letters[i]}</div>
+        <div class="contact-parting-line">
+            <hr>
+        </div>
+        <div class="contacts">
+            <button class="button-name">${twolettersName[i]}</button>
+            <div class="names">
+                <p>${contact['name']} <br> <a class="mail" href="">${contact['e-mail']}</a></p>
+            </div>
+
+        </div>
+            
+        </div>`;
         
      }
 }
