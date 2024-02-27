66976a45e6b2e12a9474a7a4a92761e764ea3054
Dimitrios Kapetanis <dimitrioskapetanis1996@gmail.com>
2024-02-27 21:57:14 +0100

    LOG-IN Section: changed some CSS + init-functions

--- summary.js
+++ summary.js
@@ -1,36 +1,32 @@
 async function initSummary() {
   await includeHTML();
-  loadGreetingName();
   await loadUserInitials();
+  await loadSummaryGreeting();
   updateMenuPoint(0);
 }
 
-async function loadGreetingName() {
-  let overlay = document.querySelector(".summary-mobile-position-content");
-  overlay.style.display = "flex";
-
+async function loadWelcomeGreeting() {
   let user = await getItem("loggedInUser");
-  console.log(user);
   let userName = user[0].name;
 
   let inputfieldMobile = document.getElementById("Greeting_Name_Mobile");
   inputfieldMobile.innerHTML = userName;
+
+  let overlay = document.querySelector(".summary-mobile-position-content");
+  overlay.style.display = "flex";
+
   setTimeout(() => {
     overlay.style.display = "none";
     overlay.style.zIndex = "-1";
-  }, 3000);
+  }, 4000);
+}
+
+async function loadSummaryGreeting() {
+  let user = await getItem("loggedInUser");
+  let userName = user[0].name;
 
   if (window.innerWidth > 1050) {
     let inputfield = document.getElementById("Greeting_Name");
     inputfield.innerHTML = userName;
   }
 }
-
-/* async function loadUserInitials() {
-  let user = await getItem("loggedInUser");
-  console.log(user);
-  let userName = user[0].name;
-  console.log(userName.charAt(0));
-
-  document.getElementById("User_Initials").innerHTML = userName.charAt(0);
-} */
