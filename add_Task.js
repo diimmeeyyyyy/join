979e7e28c87b187e6332732fbb18dde4c49715f9
EndlessLoop88 <VanillaButterfly@web.de>
2024-03-06 12:59:06 +0100

    section add task: changed function createTask() & styled CSS

--- add_Task.js
+++ add_Task.js
@@ -10,6 +10,8 @@ async function initAddTask() {
   await includeHTML();
   updateMenuPoint(1);
   await loadUserInitials();
+  const today = new Date();
+  document.getElementById('add_task_due_date').setAttribute('min', today.toISOString().substring(0, 10));
 }
 
 /* ================
@@ -36,6 +38,9 @@ async function toggleContactsDropdown() {
 
 async function renderContactsInAddTask() {
   let allContacts = await loadContacts();
+
+  // TODO If the contact list is empty, get element add-task-placeholder, change text and add a grey color (CSS class)
+
   let contactsContainer = document.getElementById("add_task_contacts_content");
   contactsContainer.innerHTML += `
     <div id="add_task_contacts_container" class="add-task-contacts-container"> 
@@ -46,12 +51,12 @@ async function renderContactsInAddTask() {
   for (let i = 0; i < allContacts.length; i++) {
     const contact = allContacts[i];
     contactList.innerHTML += `
-      <div id="add_task_contact_checkbox${i}" class="add-task-contact-checkbox"> 
+      <div id="add_task_contact_checkbox${i}" class="add-task-contact-checkbox" onclick="saveCheckedContacts(event, ${i}, '${contact.name.replace('"', '')}')"> 
         <div class="add-task-contact-icon-and-name">
             <div>${getIconForContact(contact)}</div>
             <div>${contact.name}</div>
         </div>
-        <input class="add-task-contact-check" type="checkbox" onclick="saveCheckedContacts(this.checked, '${contact.name.replace('"', '')}')">
+        <input class="add-task-contact-check" id="add_task_contact_checkbox_checkbox${i}" type="checkbox" onclick="saveCheckedContacts(event, ${i}, '${contact.name.replace('"', '')}')">
       </div>
     `;
   }
@@ -65,15 +70,22 @@ async function renderContactsInAddTask() {
 // }
 
 
-function saveCheckedContacts(isChecked, contactName) {
-  if (isChecked === true) {
-    contactsForNewTask.push(contactName);
-  } else {
-    const index = contactsForNewTask.indexOf(contactName);
-    if (index !== -1) {
-      contactsForNewTask.splice(index, 1)
-    }
+function saveCheckedContacts(event, contactIndex, contactName) {
+  const checkbox = document.getElementById(`add_task_contact_checkbox_checkbox${contactIndex}`);
+  const index = contactsForNewTask.indexOf(contactName);
+
+  if (index >= 0) {
+    contactsForNewTask.splice(index, 1);
+    checkbox.checked = false;
+    //TODO: Class hinzufügen für Farbe
   }
+  else {
+    contactsForNewTask.push(contactName);
+    checkbox.checked = true;
+  }
+
+  console.log(contactsForNewTask)
+  event.stopPropagation();
 }
 
 
@@ -100,41 +112,28 @@ function changeButtonColor() {
   let lowButton = document.getElementById("add_task_prio_button_low");
   let lowIcon = document.getElementById("add_task_prio_icon_low");
 
+  urgentButton.classList = ['add-task-prio-button'];
+  urgentIcon.classList = [];
+  mediumButton.classList = ['add-task-prio-button'];
+  mediumIcon.classList = [];
+  lowButton.classList = ['add-task-prio-button'];
+  lowIcon.classList = [];
+
   switch (prio) {
     case "urgent":
       urgentButton.classList.add("add-task-prio-button-red");
       urgentIcon.classList.add("add-task-prio-icon-white");
-      mediumButton.classList.remove("add-task-prio-button-yellow");
-      mediumIcon.classList.remove("add-task-prio-icon-white");
-      lowButton.classList.remove("add-task-prio-button-green");
-      lowIcon.classList.remove("add-task-prio-icon-white");
       break;
 
     case "medium":
       mediumButton.classList.add("add-task-prio-button-yellow");
       mediumIcon.classList.add("add-task-prio-icon-white");
-      urgentButton.classList.remove("add-task-prio-button-red");
-      urgentIcon.classList.remove("add-task-prio-icon-white");
-      lowButton.classList.remove("add-task-prio-button-green");
-      lowIcon.classList.remove("add-task-prio-icon-white");
       break;
 
     case "low":
       lowButton.classList.add("add-task-prio-button-green");
       lowIcon.classList.add("add-task-prio-icon-white");
-      urgentButton.classList.remove("add-task-prio-button-red");
-      urgentIcon.classList.remove("add-task-prio-icon-white");
-      mediumButton.classList.remove("add-task-prio-button-yellow");
-      mediumIcon.classList.remove("add-task-prio-icon-white");
       break;
-
-    default:
-      urgentButton.classList.remove("add-task-prio-button-red");
-      urgentIcon.classList.remove("add-task-prio-icon-white");
-      mediumButton.classList.remove("add-task-prio-button-yellow");
-      mediumIcon.classList.remove("add-task-prio-icon-white"); 
-      lowButton.classList.remove("add-task-prio-button-green");
-      lowIcon.classList.remove("add-task-prio-icon-white");
   }
 }
 
@@ -159,9 +158,9 @@ function addNewSubtask() {
 }
 
 function deleteSubtask() {
-  let subtask = document.getElementById('add_task_subtask_and_delete_icon'); 
+  let subtask = document.getElementById('add_task_subtask_and_delete_icon');
   indexOfSubtask = subtasks.indexOf("subtask.value");
-  subtasks.splice(indexOfSubtask,1);
+  subtasks.splice(indexOfSubtask, 1);
   subtask.remove();
   console.log('subtasks', subtasks)
 }
@@ -212,6 +211,7 @@ async function createTask() {
     dueDate: dueDate.value,
     category: category.value,
     status: "toDo",
+
   };
 
   if (description.value.trim() !== "") {
@@ -223,8 +223,21 @@ async function createTask() {
   }
 
   if (subtasks.length !== 0) {
-    task.subtasks = subtasks;
+    let newSubtask = [];
+    for (let i = 0; i < subtasks.length; i++) {
+      const subtask = subtasks[i];
+      let subtaskDetail = {
+        subtaskName: subtask,
+        checked: false
+      }
+      newSubtask.push(subtaskDetail);
+
+    }
+
+    task.subtasks = newSubtask;
   }
+
+
 
   if (prio !== "") {
     task.prio = prio;
@@ -237,7 +250,7 @@ async function createTask() {
   await setItem("taskIdCounter", task.id);
 
   console.log('allTasks', allTasks);
-  
+
   title.value = "";
   description.value = "";
   dueDate.value = "";
@@ -256,12 +269,12 @@ function clearAddTaskForm() {
   prio = "medium";
   changeButtonColor();
 
-  let newSubtasksList = document.getElementById("add-task-subtasks-list"); 
+  let newSubtasksList = document.getElementById("add-task-subtasks-list");
   newSubtasksList.innerHTML = '';
 
   let contactList = document.getElementById('add_task_contacts_content');
   contactList.style.display = 'none';
-    contactsDropdownOpen = false;
+  contactsDropdownOpen = false;
 }
 
 
